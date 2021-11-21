0b4bdc479a1ea5e69498cce6adafe613daea05c4
phawers <phawersnone@gmail.com>
2021-11-22 01:38:33 +0800

    第一次提交

---
+++ pages/courselist/courselist.js
@@ -0,0 +1,89 @@
+// pages/courselist/courselist.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    cuList: [
+      {
+        title: '课程1',
+        imgUrl:  '/resource/img/sample.png',
+        hits: 0,
+      },
+      {
+        title: '课程2',
+        imgUrl:  '/resource/img/sample.png',
+        hits: 0,
+      }
+    ],
+    tsList: [
+      {
+        title: '练习1',
+        imgUrl:  '/resource/img/sample.png',
+        hits: 0,
+      },
+      {
+        title: '练习2',
+        imgUrl:  '/resource/img/sample.png',
+        hits: 0,
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
